ef8cd63ca8e013bcffba3220ed9f0e5b2f8e7ef2
soojin <58179162+soojinme@users.noreply.github.com>
2026-03-21 01:38:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,41 +4,34 @@ const repo = "boxx";
 const downloadBtn = () => document.querySelector(".top-right .btn");
 
 async function loadFiles() {
-  const res = await fetch(`https://api.github.com/repos/${username}/${repo}/contents/files`);
-  const files = await res.json();
+  try {
+    const res = await fetch(`https://api.github.com/repos/${username}/${repo}/contents/files`);
+    const files = await res.json();
 
-  const list = document.getElementById("fileList");
-  list.innerHTML = "";
+    const list = document.getElementById("fileList");
+    list.innerHTML = "";
 
-  for (const file of files) {
-    const commitRes = await fetch(
-      `https://api.github.com/repos/${username}/${repo}/commits?path=files/${file.name}&per_page=1`
-    );
-    const commitData = await commitRes.json();
+    files.forEach(file => {
+      const div = document.createElement("div");
+      div.className = "item";
 
-    const date = new Date(commitData[0].commit.committer.date);
-    const formatted = date.toLocaleString();
-
-    const div = document.createElement("div");
-    div.className = "item";
-
-    div.innerHTML = `
-      <div class="left">
-        <input type="checkbox" class="file-check" value="${file.download_url}" onchange="updateSelectedCount()">
-
-        <div class="file-info">
-          <div>${file.name}</div>
-          <div class="time">${formatted}</div>
+      div.innerHTML = `
+        <div class="left">
+          <input type="checkbox" class="file-check" value="${file.download_url}" onchange="updateSelectedCount()">
+          <div class="file-name">${file.name}</div>
         </div>
-      </div>
 
-      <a class="download" href="${file.download_url}">Download</a>
-    `;
+        <a class="btn" href="${file.download_url}">Download</a>
+      `;
 
-    list.appendChild(div);
+      list.appendChild(div);
+    });
+
+    updateSelectedCount();
+
+  } catch (e) {
+    document.getElementById("fileList").innerText = "Failed to load";
   }
-
-  updateSelectedCount(); // 초기 상태
 }
 
 function toggleAll(master) {
@@ -55,9 +48,7 @@ function updateSelectedCount() {
   const checked = document.querySelectorAll(".file-check:checked");
   document.getElementById("selectedCount").innerText = `${checked.length}개 선택`;
 
-  // 🔥 버튼 활성/비활성
-  const btn = downloadBtn();
-  btn.disabled = checked.length === 0;
+  downloadBtn().disabled = checked.length === 0;
 }
 
 function downloadSelected() {
@@ -75,6 +66,4 @@ function downloadSelected() {
   });
 }
 
-loadFiles().catch(() => {
-  document.getElementById("fileList").innerText = "Failed to load";
-});
+loadFiles();
