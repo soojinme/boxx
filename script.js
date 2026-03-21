1d7c697d6977148c2eea74f61cb9f97bd669cc02
soojin <58179162+soojinme@users.noreply.github.com>
2026-03-21 13:41:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ const repo = "boxx";
 
 const downloadBtn = () => document.querySelector(".top-right .btn");
 
+// 🔥 파일명 분리 (확장자 유지)
+function splitName(name) {
+  const lastDot = name.lastIndexOf(".");
+  if (lastDot === -1) return { base: name, ext: "" };
+
+  return {
+    base: name.slice(0, lastDot),
+    ext: name.slice(lastDot)
+  };
+}
+
 async function loadFiles() {
   try {
     const res = await fetch(`https://api.github.com/repos/${username}/${repo}/contents/files`);
@@ -12,13 +23,18 @@ async function loadFiles() {
     list.innerHTML = "";
 
     files.forEach(file => {
+      const { base, ext } = splitName(file.name);
+
       const div = document.createElement("div");
       div.className = "item";
 
       div.innerHTML = `
         <div class="left">
           <input type="checkbox" class="file-check" value="${file.download_url}" onchange="updateSelectedCount()">
-          <div class="file-name">${file.name}</div>
+
+          <div class="file-name">
+            <span class="name-base">${base}</span><span class="name-ext">${ext}</span>
+          </div>
         </div>
 
         <a class="btn" href="${file.download_url}">Download</a>
